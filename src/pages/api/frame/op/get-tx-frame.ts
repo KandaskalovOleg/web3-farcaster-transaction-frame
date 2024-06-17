5387da028f90a1afe9add13825c0d485011bbf30
Oleh Kandaskalov <olegkandaskalov@gmail.com>
2024-06-17 22:36:40 +0300

    delete check 405status

--- src/pages/api/frame/op/get-tx-frame.ts
+++ src/pages/api/frame/op/get-tx-frame.ts
@@ -10,10 +10,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  /*
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
   }
-
+  */
   const { isValid, message } = await CoinbaseKit.validateMessage(
     req.body as FrameRequest
   );
